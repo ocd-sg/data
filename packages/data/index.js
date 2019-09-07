16be994d3a57e37489a7d2858c977c252f40714a
Yin Shanyang <yang@swarm.is>
2019-09-07 16:19:57 +0530

    fix: fix dependency names

--- packages/data/index.js
+++ packages/data/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  csv: require('@ocd-sg/csv'),
-  json: require('@ocd-sg/json'),
-  ndjson: require('@ocd-sg/ndjson')
+  csv: require('@ocd-data/csv'),
+  json: require('@ocd-data/json'),
+  ndjson: require('@ocd-data/ndjson')
 }
