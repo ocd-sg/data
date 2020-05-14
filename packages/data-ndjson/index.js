509d9aa50be2c2caf710489b1b1caf1e4a629c97
Yin Shanyang <me@yinshanyang.com>
2020-05-14 15:44:23 +0800

    chore: clean up `data-ndjson`

--- packages/data-ndjson/index.js
+++ packages/data-ndjson/index.js
@@ -1,28 +1,29 @@
 const fs = require('fs')
 
-const parse = (path) => fs.readFileSync(path, 'utf8')
-  .split('\n')
-  .map((d) => d.trim())
-  .filter((d) => d)
-  .map((d) => JSON.parse(d))
-const format = (path, data) => fs.writeFileSync(
-  path,
-  data
+const parse = (path) =>
+  fs
+    .readFileSync(path, 'utf8')
+    .split('\n')
+    .map((d) => d.trim())
     .filter((d) => d)
-    .map((d) => JSON.stringify(d))
-    .join('\n')
-)
+    .map((d) => JSON.parse(d))
+const format = (path, data) =>
+  fs.writeFileSync(
+    path,
+    data
+      .filter((d) => d)
+      .map((d) => JSON.stringify(d))
+      .join('\n')
+  )
 
 const parseRow = (row) => JSON.parse(row)
 const formatRow = (row) => JSON.stringify(row).concat('\n')
 
 const fromNDJson = (path) => {
   const { Observable } = require('rxjs')
-  const { map } = require('rxjs/operator')
   const split = require('split2')
 
-  const stream = fs.createReadStream(path)
-    .pipe(split(parseRow))
+  const stream = fs.createReadStream(path).pipe(split(parseRow))
 
   return Observable.create((observer) => {
     stream.on('data', observer.next.bind(observer))
